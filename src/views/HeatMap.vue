07d6e7fe09dab0e5c29fc8a158c0ca61fdef0d55
cihan.gurturk <cihan.gurturk@foreks.com>
2024-10-25 17:57:14 +0300

    table added , sdk version updated

--- src/views/HeatMap.vue
+++ src/views/HeatMap.vue
@@ -1,4 +1,6 @@
 <template>
+  <div @click="goBack()">BACK</div>
+  <div @click="toggleShowGraph()">Graph</div>
   <div v-if="!ready">
     <div class="lds-ripple">
       <div></div>
@@ -7,17 +9,50 @@
     <p>loading heatmap data...</p>
   </div>
   <div v-if="ready">
-    <div id="googleChart"></div>
+    <div v-if="graphShow" id="googleChart"></div>
+    <div v-if="!graphShow">
+      <div>{{ tableData.parent[0].name }}</div>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>last</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(item, index) in tableData.childs"
+            @click="tableClick(item)"
+            :key="index"
+          >
+            <td>{{ item.name }}</td>
+            <td>{{ item.last }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import HeatMapResponse from "foreks-sdk/commons/types/repo/HeatMapResponse";
 import { Options, Vue } from "vue-class-component";
 import { GoogleCharts } from "google-charts";
+import { reactive } from "vue";
 
 @Options({})
 export default class extends Vue {
+  /* eslint-disable */
   ready = false;
+  container;
+  chartData;
+  tableData;
+  data;
+  graphShow = true;
+  parentInfo;
+  parentCode;
+  scaleMax;
+  treemap;
+  selectedlevel = "XU100";
   levelCalculatedData = {};
   heatMapData!: HeatMapResponse;
   positiveColor = [
@@ -28,11 +63,11 @@ export default class extends Vue {
     "rgba(43,160,41,1)",
   ];
   negativeColor = [
-    "rgba(219,17,19,1)",
-    "rgba(219,17,19,0.8)",
-    "rgba(219,17,19,0.6)",
-    "rgba(219,17,19,0.4)",
     "rgba(219,17,19,0.2)",
+    "rgba(219,17,19,0.4)",
+    "rgba(219,17,19,0.6)",
+    "rgba(219,17,19,0.8)",
+    "rgba(219,17,19,1)",
   ];
   zeroColor = ["rgba(69, 69, 69, 0.5)"];
   scaleColors = [
@@ -40,7 +75,6 @@ export default class extends Vue {
     "rgba(0,0,0,0.1)",
     ...this.positiveColor,
   ];
-
   async mounted() {
     GoogleCharts.load("current", { packages: ["treemap"] });
     await this.initialHeatMap();
@@ -53,6 +87,31 @@ export default class extends Vue {
       this.calculateDept(this.heatMapData, "root");
       this.drawGraph();
     });
+  }
+
+  goBack() {
+    this.treemap.goUpAndDraw();
+    this.addColors();
+  }
+
+  toggleShowGraph() {
+    console.log(this.levelCalculatedData);
+    this.graphShow = !this.graphShow;
+    this.graphShow ? this.drawGraph() : this.drawTable(null,null)
+    console.log(this.tableData);
+
+  }
+
+  tableClick(item){
+    console.log(item);
+    var dataRows = this.data.getFilteredRows([
+        {
+          column: 0,
+          value: item.label,
+        },
+      ]);
+      console.log(dataRows);
+      this.drawTable(item.name,null);
   }
 
   calculateLevelsData(levelJson, parentCode) {
@@ -122,9 +181,33 @@ export default class extends Vue {
     this.drawChart();
   }
 
+  drawTable(code, obj) {
+        if (code) {
+          if (this.levelCalculatedData.hasOwnProperty(code)) {
+            this.selectedlevel = code;
+          } else {
+            if (obj) {
+              if (obj.show) {
+                obj.show = false;
+              } else {
+                obj.show = true;
+              }
+            }
+          }
+        }
+        this.graphShow = false;
+        this.tableData = this.levelCalculatedData[this.selectedlevel];
+        this.parentInfo = this.tableData.parent[0];
+        this.parentInfo.changePercentageFormatted = this.parentInfo.changePercentage;
+        this.scaleRefresh();
+      };
+      scaleRefresh() {
+        this.scaleMax = this.levelCalculatedData[this.selectedlevel].absMax;
+        this.parentCode = this.levelCalculatedData[this.selectedlevel].parent[0].parentCode;
+      };
   drawChart() {
-    var chartData = this.prepareData();
-    chartData.unshift([
+    this.chartData = this.prepareData();
+    this.chartData.unshift([
       "Code",
       "Parent",
       "Calc",
@@ -134,83 +217,112 @@ export default class extends Vue {
       "name",
     ]);
 
-    GoogleCharts.api.setOnLoadCallback(function () {
-      var data = GoogleCharts.api.visualization.arrayToDataTable(chartData);
-      var view = new GoogleCharts.api.visualization.DataView(data);
+    GoogleCharts.api.setOnLoadCallback(() => {
+      this.data = GoogleCharts.api.visualization.arrayToDataTable(this.chartData);
+      var view = new GoogleCharts.api.visualization.DataView(this.data);
+      console.log({view});
       view.setColumns([0, 1, 2]);
 
-      var container = document.getElementById("googleChart");
-      var treemap = new GoogleCharts.api.visualization.TreeMap(container);
+      this.container = document.getElementById("googleChart");
+      this.treemap = new GoogleCharts.api.visualization.TreeMap(this.container);
 
-      function drawIt() {
-        treemap.draw(view, {
+
+      const drawIt = () => {
+        this.treemap.draw(view, {
           headerHeight: 35,
           legend: false,
           fontColor: "black",
           fontSize: 10,
           height: 500,
           width: 500,
-        });
-        setTimeout(function () {
-          addColors();
+          eventsConfig: {
+            highlight: [],
+          }
+        },);
+        setTimeout(() => {
+          this.addColors();
         });
       }
 
-      function addColors() {
-        const elements = Array.from(
-          document.getElementsByTagName(
-            "rect"
-          ) as unknown as HTMLCollectionOf<HTMLElement>
-        );
-
-        elements.forEach((element, index) => {
-          var textElement = element?.parentNode?.querySelector("text");
-          if (textElement?.textContent?.includes("…")) {
-            for (var i = 0; i < data.Wf.length; i++) {
-              if (
-                data.Wf[i].c[0].v.startsWith(
-                  textElement?.textContent?.replace("…", "")
-                )
-              ) {
-                textElement!.textContent = data.Wf[i].c[0].v;
-                textElement!.setAttribute("style", "writing-mode: tb;");
-                break;
-              }
-            }
-          }
-          var dataRows = data.getFilteredRows([
-            {
-              column: 0,
-              value: textElement?.textContent,
-            },
-          ]);
-          if (dataRows.length > 0) {
-            element.setAttribute("fill", data.getValue(dataRows[0], 3));
-          }
-        });
-      }
       GoogleCharts.api.visualization.events.addListener(
-        treemap,
+        this.treemap,
         "onmouseout",
-        addColors
+        this.addColors
       );
       GoogleCharts.api.visualization.events.addListener(
-        treemap,
+        this.treemap,
         "onmouseover",
-        addColors
+        this.addColors
       );
       GoogleCharts.api.visualization.events.addListener(
-        treemap,
+        this.treemap,
         "ready",
-        addColors
+        this.addColors
       );
+      GoogleCharts.api.visualization.events.addListener(this.treemap, 'select', () => {
+        console.log(this.treemap.getSelection());
+        var selectedNode = this.treemap.getSelection()[0].row;
+        console.log(this.data.getValue(selectedNode,6));
+        selectedNode === 6 && this.treemap.setSelection([]);
+      });
       drawIt();
     });
   }
+
+  addColors() {
+    const elements = Array.from(
+      document.getElementsByTagName(
+        "rect"
+      ) as unknown as HTMLCollectionOf<HTMLElement>
+    );
+
+    elements.forEach((element, index) => {
+      var textElement = element?.parentNode?.querySelector("text");
+
+
+      for (var i = 0; i < this.data.Wf.length; i++) {
+        var parentX = textElement?.getAttribute("x");
+        var isVertical = false;
+        if (textElement?.textContent?.includes("…")) {
+          isVertical = true;
+          textElement!.setAttribute("style", "writing-mode: tb;");
+        }
+        if (
+          this.data.Wf[i].c[0].v.startsWith(
+            textElement?.textContent?.replace("…", "")
+          )
+        ) {
+          let roundedNum = this.data.Wf[i].c[4].v && typeof this.data.Wf[i].c[4].v === "number" ? Math.round(this.data.Wf[i].c[4].v * 1000) / 1000 : "0";
+          if (textElement) {
+            textElement!.innerHTML = isVertical ? `<tspan x="${parentX}" dx="5">${this.data.Wf[i].c[0].v}</tspan>
+              <tspan x="${parentX}" y="${textElement?.getAttribute("y")}" dx="-5">% ${roundedNum}</tspan>` : `<tspan x="${parentX}" dy="-5">${this.data.Wf[i].c[0].v}</tspan>
+              <tspan x="${parentX}" dy="10">% ${roundedNum}</tspan>`;;
+          }
+          var textLength = textElement?.getBBox().width;
+          var svgLength = textElement?.parentElement?.getBoundingClientRect().width;
+          if (textLength && svgLength) {
+            textLength >= svgLength &&
+              textElement!.setAttribute("font-size", "8");
+          }
+          break;
+        }
+      }
+      var dataRows = this.data.getFilteredRows([
+        {
+          column: 0,
+          value: textElement?.textContent?.split(" % ")[0].trim(),
+        },
+      ]);
+      if (dataRows.length > 0) {
+        element.setAttribute("fill", this.data.getValue(dataRows[0], 3));
+      }
+    });
+  }
+  
   prepareData() {
     var dataArr: any[] = [];
+    var excludedArray: any[] = [];
     var xu100 = this.levelCalculatedData["XU100"];
-
     dataArr.push([
       xu100.parent[0].label,
       null,
@@ -220,10 +332,12 @@ export default class extends Vue {
       xu100.parent[0].last,
       xu100.parent[0].name,
     ]);
+
     for (var i = 0; i < Object.keys(this.levelCalculatedData).length; i++) {
       var key =
         this.levelCalculatedData[Object.keys(this.levelCalculatedData)[i]];
       var other = 0;
+      var totalChange = 0;
       if (key.childs) {
         key.childs = key.childs.sort(function (a, b) {
           if (a.y > b.y) return -1;
@@ -232,30 +346,38 @@ export default class extends Vue {
         });
         for (var j = 0; j < key.childs.length; j++) {
           var child = key.childs[j];
-          if (j < 5 || child.label.indexOf("BIST") > -1) {
-            dataArr.push([
-              child.label,
-              key.parent[0].label,
-              child.y,
-              child.color,
-              child.changePercentage,
-              child.last,
-              child.name,
-            ]);
+
+          if (j < 5) {
+            if (!excludedArray.includes(key.parent[0].label)) {
+              dataArr.push([
+                child.label,
+                key.parent[0].label,
+                child.y,
+                child.color,
+                child.changePercentage,
+                child.last,
+                child.name,
+              ]);
+            }
           } else {
             other += child.y;
+            totalChange += child.changePercentage;
+            excludedArray.push(child.label);
           }
         }
         if (other !== 0) {
-          dataArr.push([
-            key.parent[0].name + " DIGER",
-            key.parent[0].label,
-            other,
-            "#bfbfbf",
-            0,
-            0,
-            "",
-          ]);
+          if (!excludedArray.includes(key.parent[0].label)) {
+            console.log(totalChange , key.parent[0].name)
+            dataArr.push([
+              key.parent[0].name + " DIGER",
+              key.parent[0].label,
+              other,
+              "#bfbfbf",
+              totalChange,
+              0,
+              "",
+            ]);
+          }
         }
       }
     }
